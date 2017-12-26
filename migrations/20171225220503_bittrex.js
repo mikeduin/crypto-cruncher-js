f59c6b026bd8352d7b677d97c2b0f669688197cf
Mike Duin <michael.s.duin@gmail.com>
2017-12-25 22:05:35 -0800

    add bittrex migration

--- migrations/20171225220503_bittrex.js
+++ migrations/20171225220503_bittrex.js
@@ -1,6 +1,5 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('hitbtc', function(t){
+  return knex.schema.createTable('bittrex', function(t){
     t.increments();
     t.string('mySymbol');
     t.string('mktSymbol');
@@ -11,5 +10,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('hitbtc');
+  return knex.schema.dropTable('bittrex');
 };
