3ecaa41f6d4b738f04c9fa416510d84380e63552
Mike Duin <michael.s.duin@gmail.com>
2018-01-02 13:58:08 -0800

    update db env var

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,13 @@
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/crypto-cruncher'
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+    // connection: 'postgres://localhost/crypto-cruncher'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
-}
+
+};
