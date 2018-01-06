a1c348e2db128b1dc34fa2bee13deac5dba53ecf
Mike Duin <michael.s.duin@gmail.com>
2018-01-06 03:02:32 -0800

    add auth controller

--- public/javascripts/controllers/auth.controller.js
+++ public/javascripts/controllers/auth.controller.js
@@ -1,7 +1,24 @@
 angular
   .module('cryptoCruncher')
-  .controller('AuthController', AuthController)
+  .controller('AuthController', ['$state', 'authService', AuthController])
 
-function AuthController () {
+function AuthController ($state, authService) {
+  var vm = this;
 
-}
+  vm.register = function(user) {
+    authService.register(user).error(function(error){
+      vm.error = error.message;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  vm.login = function(user){
+    authService.login(user).error(function(error){
+      vm.error = error.message;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+};
