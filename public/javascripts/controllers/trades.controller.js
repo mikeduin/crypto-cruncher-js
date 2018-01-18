0139301c4038747e7cb963ee7f6936fa214e5206
Mike Duin <michael.s.duin@gmail.com>
2018-01-18 03:49:19 -0800

    add find currency fn in partial view/cont

--- public/javascripts/controllers/trades.controller.js
+++ public/javascripts/controllers/trades.controller.js
@@ -12,6 +12,7 @@ function TradeController ($state, tradeService, marketService) {
   vm.tradeParts = 'part';
   vm.tradeDir = 'openAdd';
   vm.arbType = 'immed';
+  vm.symbolSell = 'BTC (Bitcoin)';
   vm.partsView = false;
   vm.directionView = false;
   vm.arbView = false;
@@ -23,8 +24,52 @@ function TradeController ($state, tradeService, marketService) {
     total: 0,
     method: 'bank',
     exchange: vm.exchanges[0],
+    curr_sold: 'BTC',
     time: new Date(2017, 0, 1, 12, 00, 0),
     date: new Date()
+  };
+
+  vm.feeObject = {
+    'deposit': {
+      'coinbase': {
+        'bank': 0,
+        'card': 0
+      }
+    },
+    'buy': {
+      'coinbase': {
+        'bank': 0.0149,
+        'card': 0.0399
+      }
+    },
+    'sell': {
+      'coinbase': {
+        'bank': 0.0149,
+        'card': 0.0399
+      }
+    }
+  };
+
+  (function getSymbols () {
+    marketService.getSymbols().then(function(res){
+      vm.symbolIndex = res;
+    })
+  })();
+
+  vm.findCurrency = function (symbol) {
+    var found = false;
+    var lower = symbol.toLowerCase();
+    for (var i=0; i<vm.symbolIndex.length; i++) {
+      if (vm.symbolIndex[i].symbol.toLowerCase() == lower) {
+        vm.symbolBuy = vm.symbolIndex[i].full_name;
+        found = true;
+      };
+    };
+    if (symbol == undefined || symbol == null || symbol == ''){
+      vm.symbolBuy = '';
+    } else if (!found && symbol !== undefined) {
+      vm.symbolBuy = "Currency not found!";
+    };
   };
 
   vm.tradeSubmit = function(trade) {
@@ -107,6 +152,7 @@ function TradeController ($state, tradeService, marketService) {
     } else if (vm.tradeType === "reset") {
       vm.directionView = false;
       vm.partsView = false;
+      vm.typeDesc = '';
       dirView();
       $state.go('home.addTrade');
     }
